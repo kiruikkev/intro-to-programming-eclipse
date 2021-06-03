9b10a5741df28e776d9f51d6e3cc3b8237336b2d
kiruikkev@gmail.com <kiruikkev@gmail.com>
2021-06-03 17:44:54 -0400

    Exercise-4-2 update on profile

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-const today = new Date();
-const thisYear = today.getFullYear();
-const footer = document.querySelector('footer');
-const copyright = document.createElement('p');
-copyright.innerHTML = ("Kelvin Kirui @Copy:" + thisYear);
-footer.appendChild(copyright);
-
-
-
-let skills = ['Java script', 'HTML', 'Linux', 'Windows']
-
-function addSkills(skills) {
-    for (let i = 0, i < skills.length; i++) {
-        let skill = document.createElement('li');
-        skill.innerHTML = (skills[i]);
-        skilllist.appendChild(skill);
-
-    }
-
-
+const today = new Date()
+console.log(today)
+const thisyear = today.getFullYear()
+console.log(thisyear)
+const footer = document.querySelector('footer')
+console.log(footer)
+const copyright = document.createElement('p')
+console.log(copyright)
+copyright.innerHTML = `Kelvin kirui &copy; ${thisyear}`;
+footer.appendChild(copyright)
+const skills = [
+    'Java script', 'HTML', 'Linux', 'Windows'
+]
+const skillsSection = document.getElementById("skills")
+const skillsList = skillsSection.querySelector('ul')
+console.log(skills)
+for (let i = 0; i < skills.length; i++) {
+    const skill = document.createElement('li');
+    skill.innerHTML = skills[i];
+    console.log(skill.innerHTML)
+    skillsList.appendChild(skill)
 }
-addSkills(skills);
