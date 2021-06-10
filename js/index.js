a1d343eb31565c94479b2112216211a55ff358cb
kiruikkev@gmail.com <kiruikkev@gmail.com>
2021-06-09 20:39:29 -0400

    lesson-4-3 more profile updates

--- js/index.js
+++ js/index.js
@@ -1,22 +1,53 @@
-const today = new Date()
-console.log(today)
-const thisyear = today.getFullYear()
-console.log(thisyear)
-const footer = document.querySelector('footer')
-console.log(footer)
-const copyright = document.createElement('p')
-console.log(copyright)
+const today = new Date();
+console.log(today);
+const thisyear = today.getFullYear();
+console.log(thisyear);
+const footer = document.querySelector('footer');
+console.log(footer);
+const copyright = document.createElement('p');
+console.log(copyright);
 copyright.innerHTML = `Kelvin kirui &copy; ${thisyear}`;
-footer.appendChild(copyright)
+footer.appendChild(copyright);
 const skills = [
     'Java script', 'HTML', 'Linux', 'Windows'
-]
-const skillsSection = document.getElementById("skills")
-const skillsList = skillsSection.querySelector('ul')
-console.log(skills)
+];
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.querySelector('ul');
+console.log(skills);
 for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement('li');
     skill.innerHTML = skills[i];
-    console.log(skill.innerHTML)
-    skillsList.appendChild(skill)
+    console.log(skill.innerHTML);
+    skillsList.appendChild(skill);
 }
+
+
+
+
+const messageForm = document.getElementById('leave_message');
+console.log(messageForm)
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const Name = e.target.name.value;
+    const Email = e.target.email.value;
+    const Message = e.target.message.value;
+
+    console.log(Name, Email, Message);
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `<a href="mailto:${Email}">${Name}</a> wrote:<span>${Message}</span>`
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (event) => {
+        const entry = event.target.parentNode;
+        entry.remove()
+    })
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+
+});
